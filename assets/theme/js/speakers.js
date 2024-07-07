256ee6cdef9ab74fe3cf13a47a05000fc8e791d7
Mercia Malan <malan747@gmail.com>
2024-07-07 18:56:43 +0200

    Update speakers 2024

--- assets/theme/js/speakers.js
+++ assets/theme/js/speakers.js
@@ -19,20 +19,18 @@ Before his career in data, James was an avid musical theatre student and musicia
 {
     name: "Dale Nunns",
     bio: "I built a talking duck!  \
-          \
-          I'm a frequent speaker at BSides Cape Town and DevConf. \
-          \
-          I’ve been professionally developing software since 2000. I’m comfortable in multiple development environments and platforms and have worked on everything from big data distributed systems and large retail in-store and stock-take systems written in .NET to Linux kernel drivers and code running on 8-bit micro-controllers.",
+          <br />\
+          <br />I'm a frequent speaker at BSides Cape Town and DevConf. \
+          <br />\
+          <br />I’ve been professionally developing software since 2000. I’m comfortable in multiple development environments and platforms and have worked on everything from big data distributed systems and large retail in-store and stock-take systems written in .NET to Linux kernel drivers and code running on 8-bit micro-controllers.",
     title: "I can see clearly now - Adding vision to a rubber duck",
     talk: "Adding better vision capabilities to my AI Robot Rubber Duck, Miss Duckworth. \
-\
-Miss Duckworth is a large 3D printed duck that is capable of having discussions with you and runs all of the AI models locally without internet access. \
-\
-Using machine learning, various image recognition libraries and multimodal AI models I want to add basic vision capabilities to the duck. \
-\
-Be able to ask it questions about what it 'sees' and have the duck answer back. Have the duck recognise people and try have conversations with them. \
-\
-This talk covers how I got this to work, if it works, and all the tools and libraries required to do image processing offline using low powered computers without GPU's etc.",
+<br /> \
+<br /> Miss Duckworth is a large 3D printed duck that is capable of having discussions with you and runs all of the AI models locally without internet access. \
+<br /> Using machine learning, various image recognition libraries and multimodal AI models I want to add basic vision capabilities to the duck. \
+<br /> Be able to ask it questions about what it 'sees' and have the duck answer back. Have the duck recognise people and try have conversations with them. \
+<br /> \
+<br /> This talk covers how I got this to work, if it works, and all the tools and libraries required to do image processing offline using low powered computers without GPU's etc.",
     tracks: "Machine Learning Engineering, Data Visualisation, Other"
 },
 {
@@ -47,20 +45,19 @@ This talk covers how I got this to work, if it works, and all the tools and libr
     bio: "Garett is a Data Scientist in Heineken's Africa and Middle East Analytics Hub, where he focuses on deploying AI solutions across multiple countries. He has also leveraged his knowledge gained from his master’s degree to contribute to use cases in global financial forecasting.",
     title: "Heinekens MLOps Journey: From notebooks to scalable products",
     talk: "Join us to discover how Heineken’s Africa and Middle East Analytics Hub transformed its approach to data and AI solutions, improving collaboration, code quality, and project delivery cadence. \
-\
-We will be discussing: \
-\
-How we addressed siloed operations and lack of standardisation in our processes\
-The development of a framework to address these issues\
-The benefits realised by the adoption of this framework\
-How we see the framework evolving in the future",
+<br />\
+<br />We will be discussing: \
+<br />How we addressed siloed operations and lack of standardisation in our processes\
+<br />The development of a framework to address these issues\
+<br />The benefits realised by the adoption of this framework\
+<br />How we see the framework evolving in the future",
     tracks: "Data Science, Managing data teams, Other"
 },
 {
     name: "Georgina Armstrong",
     bio: "Georgina has been working in Data Science since 2011. For many years she was a coding data scientist in R, Python and PySpark, but for the last 5 years she's been in leadership. She's been a manager through several hiring and layoff cycles in different companies and industries, and now she is Head of Data for a listed company in the United States. She's figured out how to keep some very different teams happy, productive and collaborative in Data Science, Data Engineering and Advanced Analytics disciplines. She definitely learned some stuff the hard way, and wants to pass that on so that other people don't have to.",
     title: "Managing Multinational Data teams.",
-    talk: "I been managing a team of six data scientists, engineers and analysts, spanning four countries and six nationalities for two years. Here is what I have learned.",
+    talk: "I been managing a team of six data scientists, engineers and analysts, spanning four countries and six nationalities for two years. <br /><br />Here is what I have learned.",
     tracks: "Managing data teams"
 },
 {
@@ -70,24 +67,20 @@ How we see the framework evolving in the future",
 As Einstein might have said, the definition of insanity is re-publishing the same Tableau dashboard over and over again and expecting a different outcome. I have done plenty of that lately and I have some learnings to share. ",
     title: "Applying engineering principles to reporting",
     talk: "In many companies, business level reporting is used to drive executive decisions. More often than not, not enough attention is paid to how those reports are built, maintained, made performant and resilient to change. \
-\
-In this session, I will talk about my team's experiences in bringing engineering principles normally applied to data pipelines all the way to the reporting layer (specifically Tableau).\
-\
-Using existing APIs, we are able to do interesting things.\
-\
-Fist, discover how far the reporting landscape go (how big is the bus factor) so we can refactor reports in line with software engineering practices. \
-\
-Second, treat reporting as artefacts that we can manage by an automated pipeline (a la CI/ CD).\
-\
-Third, expand data quality checks to the reporting layer so our business consumers can be notified of quality issues as soon as they occur. \
-\
-And last but not least, bring the computation of business metrics back to the database.",
+<br /> \
+<br /> In this session, I will talk about my team's experiences in bringing engineering principles normally applied to data pipelines all the way to the reporting layer (specifically Tableau).\
+<br /> \
+<br /> Using existing APIs, we are able to do interesting things.\
+<br /> - Fist, discover how far the reporting landscape go (how big is the bus factor) so we can refactor reports in line with software engineering practices. \
+<br /> - Second, treat reporting as artefacts that we can manage by an automated pipeline (a la CI/ CD).\
+<br /> - Third, expand data quality checks to the reporting layer so our business consumers can be notified of quality issues as soon as they occur. \
+<br /> - And last but not least, bring the computation of business metrics back to the database.",
     tracks: "Data Engineering, Business Intelligence"
 },
  {
     name: "Kabelo Masemola",
     bio: "I serve as the Chief data and Analytics officer at Moniepoint (Africa's fastest growing fintech), and have years of experience designing data strategies and have dabbled in building machine learning models that are currently running in production systems",
-    title: "a practical approach to federated data governance strategies",
+    title: "A practical approach to federated data governance strategies",
     talk: "The talk will be taking the audience through data governance strategy implementation with a specific view of data mesh supported governance. We will also discuss how this approach drives innovation",
     tracks: "Data governance, security and privacy, Managing data teams, Data Ops"
 },
@@ -96,7 +89,8 @@ And last but not least, bring the computation of business metrics back to the da
     bio: "Michael Johnson is a Business Intelligence architect and Microsoft data platform MVP living in Johannesburg, South Africa. Michael has been working with data for the last 15 years and has run the local Data Platform User Group and SQL Saturday conference event for the last few years.\
 He enjoys showing people new tools and technologies that allow them to work more effectively with their data. ",
     title: "5 types of databases in less than 40 minutes",
-    talk: "For over 40 years, the relational database has been the de facto database architecture used by organizations large and small to power their applications. more recently we have seen other alternatives emerge that could be better suited to the application requirements. This talk will look at 5 types of databases that may be better alternatives. ",
+    talk: "For over 40 years, the relational database has been the de facto database architecture used by organizations large and small to power their applications. more recently we have seen other alternatives emerge that could be better suited to the application requirements. \
+    <br /> <br />This talk will look at 5 types of databases that may be better alternatives. ",
     tracks: "Data Engineering"
 },
 {
@@ -104,15 +98,15 @@ He enjoys showing people new tools and technologies that allow them to work more
     bio: "I head up intern and graduate development initiatives at Sand technologies. I am passionate about helping junior data professionals discover themselves and find their place in teams they land in. My role has given me insights on the value of having a strong technical foundation, emphasis on soft skills development, and providing mentorship opportunities.",
     title: "Building strong foundations: Designing effective development programs for junior data talent",
     talk: "We are all familiar with the challenge of being a new graduate struggling to secure a job due to the requirement for work experience. However, without someone willing to provide that initial opportunity, gaining the necessary experience remains elusive. In the data industry, this issue is further complicated by the various non-traditional paths students can take to acquire skills, such as bootcamps and online courses. While these avenues may equip students with technical knowledge, being an effective team member demands much more. So, how can those of us who work with junior talent design programmes that bridge this gap?\
-\
-My talk will focus on my experience designing programmes that develop junior data professionals into well-rounded individuals. I will explore the following key areas:\
-\
-- Design for the future: Today’s graduates will eventually become managers, and technical expertise alone does not create impactful and inspiring leaders. How can we start cultivating leadership qualities to prepare juniors for effectiveness in future roles?\
-- Recognising the unknown: New professionals often feel the need to overwork to prove their worth, adopting a jack-of-all-trades approach while mastering very little. Instead of pursuing multiple paths, how can we guide them towards a focused direction and refine their learning process?\
-- Learning how to learn: The data industry is constantly evolving, making continuous learning an essential skill. How can we equip graduates with future-proof skills that will form the foundation of their data careers?\
-- Developing professional skills remotely: Remote work removes the opportunity to learn by observing others in person. How can we still teach professional behaviour virtually?\
-- Balancing technical and soft skills: While technical skills are crucial for data professionals, being a successful team member requires a lot of soft skills. How do we strike a balance between technical development and personal growth and emphasise the value of both?\
-- It takes a village: Starting a career can be an overwhelming experience, but success is more likely when supported by a community of relatable peers. How can we foster such communities to support junior professionals on their journey and help them build their professional network at the same time?",
+<br />\
+<br/>My talk will focus on my experience designing programmes that develop junior data professionals into well-rounded individuals. I will explore the following key areas:\
+<br />\
+<br />- Design for the future: Today’s graduates will eventually become managers, and technical expertise alone does not create impactful and inspiring leaders. How can we start cultivating leadership qualities to prepare juniors for effectiveness in future roles?\
+<br />- Recognising the unknown: New professionals often feel the need to overwork to prove their worth, adopting a jack-of-all-trades approach while mastering very little. Instead of pursuing multiple paths, how can we guide them towards a focused direction and refine their learning process?\
+<br />- Learning how to learn: The data industry is constantly evolving, making continuous learning an essential skill. How can we equip graduates with future-proof skills that will form the foundation of their data careers?\
+<br />- Developing professional skills remotely: Remote work removes the opportunity to learn by observing others in person. How can we still teach professional behaviour virtually?\
+<br />- Balancing technical and soft skills: While technical skills are crucial for data professionals, being a successful team member requires a lot of soft skills. How do we strike a balance between technical development and personal growth and emphasise the value of both?\
+<br />- It takes a village: Starting a career can be an overwhelming experience, but success is more likely when supported by a community of relatable peers. How can we foster such communities to support junior professionals on their journey and help them build their professional network at the same time?",
     tracks: "Managing data teams, Other"
 }
 ];
@@ -123,7 +117,7 @@ $(document).ready(function() {
     var modal = $("#speakerModal").clone(); 
     var speaker_html = "<h5>" + modalData[i].name + "</h5>" + modalData[i].bio 
     modal.attr("id", "speakerModal" + first_name); 
-    modal.find(".modal-img").html("<img src=\"assets/images/speakers_2024/speaker_" + first_name.toLowerCase() + ".jpg\" />"); 
+    modal.find(".modal-img").html("<img src=\"assets/images/speakers_2024/speaker_" + first_name.toLowerCase() + ".png\" />"); 
     modal.find(".modal-title").text(modalData[i].title); 
     modal.find(".modal-speaker-bio").html(speaker_html); 
     modal.find(".modal-talk-content").html(modalData[i].talk);
