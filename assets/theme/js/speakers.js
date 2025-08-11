08b0cd6122b2f4bf252d7f6d1cce6937bfc92ca6
Mercia Malan <mmalan@sandtech.com>
2025-08-11 17:58:36 +0200

    update speaker bios

--- assets/theme/js/speakers.js
+++ assets/theme/js/speakers.js
@@ -1,70 +1,71 @@
 var modalData = [
 {
     name: "Melissa Albeldas",
-    bio: "Speaker bio to be added",
+    bio: "Passionate data scientist and rock climber.. ",
     title: "Climbing Out of Burnout: Lessons from Indoor Rock Climbing, MLOps, and the Data Science Lifecycle",
     talk: "This talk is going to cover a really important topic in tech...burnout. I will use MLOps and Indoor Rock climbing as 2 metaphors for burnout and then talk about how AI can be used to help with MLOps, rock climbing, and burnout. Part 1 will cover burnout and how it appears in humans, part 2 will cover how we see it in other areas MLOps/climbing, and part 3 will cover how we can incorporate AI to help us with MLOps and burnout too.",
     tracks: "AI (Including GenAI), Data Science, Data Visualisation, Data Ops, Data Ethics"
 },
 {
     name: "Innocent Jackson",
-    bio: "Speaker bio to be added",
+    bio: "I am the founder of Senior Tech Hub, an initiative dedicated to empowering older adults in South Africa with practical digital skills and confidence to navigate today's technology landscape. With a background in hospitality, community development, and ethical technology education, I bring a grounded, people-first perspective to AI conversations. My experience working hands-on with seniors offers a unique view into the real-world impacts of AI, digital exclusion, and what ethical AI should truly look like when it includes all generations.",
     title: "The Forgotten Demographic: Why Seniors Matter in Ethical AI and Digital Inclusion",
     talk: "While AI ethics conversations often highlight race, gender, and geography, age is often missing. This talk explores how seniors—holders of deep wisdom—are being left behind in our rapidly evolving digital world, even as many show a strong interest in learning and engaging with technology. Through my work with Senior Tech Hub, a grassroots initiative in South Africa, I'll share real-world experiences from teaching digital literacy to older adults. The session will unpack the barriers seniors face in accessing, trusting, and interacting with AI-driven platforms—and what that reveals about our systems. This talk makes the case that building ethical, inclusive AI requires active intergenerational inclusion, and that seniors must be seen as collaborators—not afterthoughts—in designing safer, more human-centered digital ecosystems.",
     tracks: "AI (Including GenAI), Data Ethics"
 },
 {
     name: "Georgina Armstrong",
-    bio: "Speaker bio to be added",
+    bio: "Georgina has been working in Data Science since 2011. For many years she was a coding data scientist in R, Python and PySpark, but for the last five years she's been in management, the last three as been Head of Data for a listed company in the United States She's led three large recommender projects in different companies from start to finish, as well as consulting on recommender projects at other companies. She definitely learned some stuff the hard way, and wants to pass that on so that other people don't have to.",
     title: "Hard-learned lessons about Recommender Systems",
     talk: "I've built recommender systems for Shoprite, Woolworths, 22seven (personal finance) and my current employer Zedge (a gigantic mobile content app). I've learned some really interesting things about this type of data product. This talk has 20% focus on the technical approach, but 80% on insights, ethical issues and practical challenges.",
     tracks: "Data Science, Data Ethics"
 },
 {
     name: "Malcolm Hillebrand",
-    bio: "Speaker bio to be added",
+    bio: "Malcolm is a lecturer at the University of Cape Town, whose research applies maths, stats and computers to biological physics. His PhD is in Applied Maths, and he's done postdoc research in computational biochemistry (at UCT) and biophysics (in Dresden, Germany). He enjoys talking about science as much as doing it, and apart from university teaching has spoken at several international conferences as well as giving accessible and light-hearted public talks.",
     title: "Help, my data crawled off the table! A peek into how data science for biology works",
     talk: "Experiments in biology are messy and difficult. But they produce data that we as scientists can use to understand development, diseases and DNA -- after a whole lot of analysis. Join me on a behind-the-scenes journey of biophysics data analysis and modelling. Gaze in wonder at the tools and hacks we use to turn raw (and occasionally boiled) data into meaningful numbers and visualisations, using anecdotes and examples from current research.",
     tracks: "Data Science, Data Visualisation, Other"
 },
 {
     name: "Pippa Hillebrand",
-    bio: "Speaker bio to be added",
+    bio: "Pippa is a Senior Backend and DevOps Engineer working with amazee.ai to bring sovereign and dedicated AI hosting to highly regulated industries. She has been accidentally breaking software for long enough to learn how to fix it, and now writes about it too. Pippa uses AI agents to help build software solutions, but prefers the process of Test Driven Vibe Coding.",
     title: "Privacy or piracy in AI?",
     talk: "In the years before the LLM hype-train took over our lives, the hot topic was GDPR and POPIA. Keeping confidential data within secure boundaries was important to all engineering teams. LLMs blasted through those concerns like a shotgun through a paperback, and now security operators are scrambling to pick up the pieces. Barbara in sales doesn't understand why she shouldn't upload her confidential reports into ChatGPT for analysis. Thembi in admissions at the hospital doesn't understand the risks of asking ChatGPT to help her respond to a particularly angry email from a patient. Both of them have heard about the power of AI, and they have seen how much the tools can help them do their jobs better, but IT security is dealing with subtle (or explicit) data leaks. Data privacy in the age of AI is still maturing. We look at the how and why of maintaining a secure handle on the data. What should, and shouldn't, be logged. And how building an intuitive and secure product can open up more industries to the options afforded them by LLMs.",
     tracks: "AI (Including GenAI), Data governance, security and privacy"
 },
 {
     name: "Henrico Bekker",
-    bio: "Speaker bio to be added",
+    bio: "My journey into the world of artificial intelligence has been driven by a deep fascination with its potential and, increasingly, a recognition of the critical need for its security. Over time, I've had the privilege of earning multiple AI certifications, which have provided a strong foundation in the theoretical underpinnings of this rapidly evolving field. On a daily basis, I work to translate these theories into practice, designing AI systems for various customers. This hands-on experience offers a unique perspective on the intricate process of building intelligent solutions and the subtle vulnerabilities that can emerge along the way, and my involvement in internal testing of these systems allows me to actively probe for weaknesses, and a practical understanding of AI's security landscape. Beyond current applications, my engagement in research means I'm constantly exploring the frontiers of AI security. I aim to share these insights, hopefully offering a practical and forward-looking perspective on securing the AI revolution.",
     title: "Beyond the Firewall: Securing the AI Revolution",
     talk: "Join us to gain a comprehensive understanding of the emergent threats confronting AI-driven systems and to explore actionable frameworks for fostering secure AI development and deployment. This session aims not only to illuminate the challenges but also to empower attendees with the knowledge to safeguard the integrity and resilience of the AI revolution.This session won't leave you with amazing new ideas for bots, agents, LLMs or prompt engineering, but will be aimed at the fundamentals of safeguarding and monitoring all the buzzwords and acronyms.",
     tracks: "AI (Including GenAI), Data governance, security and privacy, Data Ethics"
 },
 {
     name: "Michael Johnson",
-    bio: "Speaker bio to be added",
+    bio: "Michael Johnson is a Microsoft Data Platform MVP and co-lead of the Johannesburg Data Platform User Group. Professionally, he is the owner and principal consultant at Cobalt Analytics, based in Johannesburg, South Africa. Michael regularly presents at many local and international user groups and conferences on topics including data engineering and analytics.",
     title: "Pipes and Streams: A New Age of Data Engineering Architectures",
     talk: "In this 40-minute presentation we examine the architectural evolution of data engineering. Starting with traditional batch-based pipelines through to modern streaming architectures that leverage distributed processing and stream first designs using unified data platforms such as Lakehouse.",
     tracks: "Data Engineering"
 },
 {
     name: "Carike Blignaut-Staden",
-    bio: "Speaker bio to be added",
+    // bio: "Carike Blignaut-Staden has a passion for using technology to help others. She is a DevOps engineer at OUTsurance and co-organiser of the JHBMSDUG developer community. Continuously improving and broadening her skillset, she holds multiple certifications from various disciplines such as AI, Cloud technologies, Cybersecurity, DevOps, IoT, and Site Reliability Engineering. Away from the keyboard she loves playing sports, fishing and just being in nature. She is a former provincial hockey player and national karateka. Dedicated to helping others upskill and sharing her knowledge. Connect with her on LinkedIn (https://www.linkedin.com/in/carike) or X (@CarikeBStaden)",
+    bio: "Bio coming soon",
     title: "Observability as a team sport : Designing for humans, not just metrics",
     talk: "In a world overflowing with dashboards, why do so many go unused? This talk explores how observability is not just a DevOps concern but a collaborative effort across teams. We'll dive into the principles of designing dashboards that are not only functional but also accessible, inclusive, and actionable. Learn how to build visualizations that serve real user needs, foster shared ownership, and drive better decision-making across your organization.",
     tracks: "Business Intelligence, Data Visualisation, Other"
 },
 {
     name: "Lisema Matsietsi",
-    bio: "Speaker bio to be added",
+    bio: "Lisema Matsietsi is a distinguished author and a trailblazer in the finance sector, boasting two published books. He is a prominent member of the Institute of Directors (IoDSA) and a Chartered Development Financial Analyst (CDFA). As the president of CIDEF Chapter South Africa NPC and the founder and Managing Director of Being An Analyst, Lisema is on a mission to revolutionise the way analysts are trained and empowered. His extensive banking experience spans nearly two decades, allowing him to bring a wealth of expertise to every engagement. Lisema's passion for mentorship shines through his various platforms, including the 'Becoming An Expert Analyst' book series, the insightful Weekly Analyst Newsletter, and his engaging YouTube channel and podcast. He is dedicated to nurturing the next generation of analysts, especially in South Africa, by equipping them with essential skills to thrive in the digital economy and empowering small businesses to harness the potential of AI and Industry 5.0. Lisema Matsietsi is not just a finance professional; he is a catalyst for change, committed to making a lasting impact in his field and the broader community.",
     title: "How to Craft Teams of Exceptional Analysts",
     talk: "In today's fast-paced data-driven world, building high-performing analyst teams is no longer optional; it's essential. This talk introduces ten proven models for team effectiveness, drawn from the book \"Crafting Teams of Exceptional Analysts,\" offering leaders a practical roadmap for forming, leading, and elevating analytical teams. Attendees will learn how to quickly diagnose team challenges, apply targeted strategies, and foster collaboration using frameworks like FSNPA, GRPI, Lencioni's Five Dysfunctions, and the Google Model. Whether you're leading a new team or revitalising an existing one, this session will show how pairing these models with mentorship, especially from immediate managers, can accelerate growth and unlock transformational leadership. Join us to discover how intentional model selection and adaptive leadership can turn good analysts into exceptional teams.",
     tracks: "Managing data teams"
 },
 {
-    name: "WIZA MSUKU",
-    bio: "Speaker bio to be added",
+    name: "Wiza Msuku",
+    bio: "With over a decade of experience applying data to drive impactful change in both non-profit and profit-making sectors, I have led data initiatives across public health, telecommunications and finance. My work has consistently focused on transforming raw data into actionable insights that inform strategic decisions, improve operations and create value for organizations. From implementing large-scale data analytics ecosystems to developing predictive models with Machine Learning, I bring a practical, results-driven approach to data science that aligns with organizational goals, revenue growth and community impact.",
     title: "From chaos to coherence: My journey through Offline Healthcare analytics from a hundredfold sources",
     talk: "In many resource-limited environments, the path to meaningful analytics and insights often begins in chaos. This talk shares a personal journey of transforming a fragmented and manual data extraction and aggregation process into a scalable, near real-time analytics pipeline. Starting with the labor-intensive collection of full MySQL dumps from over two hundred remote clinics via SSH-RSYNC, clinical data was manually restored into individual database schemas for analysis then subsequently transformed into a cumulative target database used for analytics, an approach riddled with inefficiencies, inconsistencies and significant delays. Over time, this process evolved into a robust data streaming architecture using solid queue background jobs and consolidated schemas, enabling near real-time insights and automated data quality checks. This session explores the technical decisions, data infrastructure changes, architectural shifts and lessons learned, highlighting how embracing change, rethinking dataflows and simplifying complexity can turn disjointed chaos into coherent, actionable intelligence. Whether you are at the beginning or mid-point of your own data journey, this talk offers practical insights for building modern pipelines that empower timely and evidence-based decision-making.",
     tracks: "Data Engineering, Data Science"
